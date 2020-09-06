db436ccf3bf9f15f018ef3610ca71c60370d0ad3
cjjland76 <cjjland76@yahoo.co.jp>
2020-09-06 21:41:05 +0900

    comment機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :videos
   devise_for :users
+  resources :videos
+  resources :comments
 
   root "videos#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
