44099508d594b68553ca64c3955c1a011a98b9af
cjjland76 <cjjland76@yahoo.co.jp>
2020-09-07 20:36:58 +0900

    comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,21 @@ class CommentsController < ApplicationController
   before_action  :authenticate_user!
   def create
     @comment = current_user.comments.create(comment_params)
+    if @comment.save
+      redirect_back(fallback_location: root_path) 
+    else
+      redirect_back(fallback_location: root_path) 
+    end
+ 
     @video = @comment.video
   end
   
   def destroy
     @comment = current_user.comments.find params[:id]
     @video = @comment.video
-    @comment.destroy
+    if @comment.destroy
+      redirect_back(fallback_location: root_path) 
+    end
   end
 
   private
