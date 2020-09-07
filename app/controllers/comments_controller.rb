9ecb513c1b4323f02d988f4d3b144158ef7ab7ec
cjjland76 <cjjland76@yahoo.co.jp>
2020-09-07 21:07:28 +0900

    jquery導入

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,21 +2,15 @@ class CommentsController < ApplicationController
   before_action  :authenticate_user!
   def create
     @comment = current_user.comments.create(comment_params)
-    if @comment.save
-      redirect_back(fallback_location: root_path) 
-    else
-      redirect_back(fallback_location: root_path) 
-    end
- 
     @video = @comment.video
+    @comment.save
   end
   
   def destroy
     @comment = current_user.comments.find params[:id]
     @video = @comment.video
-    if @comment.destroy
-      redirect_back(fallback_location: root_path) 
-    end
+    @comment.destroy
+     
   end
 
   private
